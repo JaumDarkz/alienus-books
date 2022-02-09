88ef10db9ad8327e2a883b91876296b3419f0de0
João Vinícius <97064828+JaumDarkz@users.noreply.github.com>
2022-02-09 04:12:55 -0300

    Add files via upload

---
+++ src/pages/Book.js
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  AsyncStorage,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Book = (navigation) => {
+  // Coleta Dados
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [photo, setPhoto] = useState();
+
+  // Valida se existe dados no formulario
+  const isValid = () => {
+    if (title !== undefined && title !== '') {
+      return true;
+    }
+
+    return false;
+  };
+  // Salva no DB
+  const onSave = async () => {
+    if (isValid) {
+      const id = 1;
+
+      const data = {
+        id,
+        title,
+        description,
+        photo,
+      };
+      await AsyncStorage.setItem('books', JSON.stringify(data));
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.pageTitle}>Inclua seu novo livro</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Título"
+        value={title}
+        onChangeText={(text) => {
+          setTitle(text);
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Descrição"
+        multiline={true}
+        numerOfLines={4}
+        value={description}
+        onChangeText={(text) => {
+          setDescription(text);
+        }}
+      />
+
+      <TouchableOpacity style={styles.cameraButton}>
+        <Icon name="photo-camera" size={18} color="#fff" />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.saveButton, !isValid() ? styles.saveButtonInvalid : '']}
+        onPress={onSave}>
+        <Text style={styles.saveButtonText}>Cadastrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.cancelButton}
+        onPress={() => {
+          navigation.goBack();
+        }}>
+        <Text style={styles.cancelButtonText}>Cancelar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  pageTitle: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  input: {
+    fontSize: 16,
+    borderBottomColor: '#f39c12',
+    borderBottomWidth: 1,
+    marginBottom: 10,
+  },
+  cameraButton: {
+    backgroundColor: '#f39c12',
+    borderRadius: 50,
+    width: 32,
+    height: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  saveButton: {
+    backgroundColor: '#f39c12',
+    alignSelf: 'center',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+  saveButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  saveButtonInvalid: {
+    opacity: 0.5,
+  },
+  cancelButton: {
+    alignSelf: 'center',
+  },
+  cancelButtonText: {
+    color: '#95a5a6',
+  },
+});
+
+export default Book;
